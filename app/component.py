5cd8fbfe4470fa5a582f5f09efcfba1542a73fcc
fahmizainal17 <fahmizainal9@gmail.com>
2024-10-14 14:24:57 +0800

    added files

--- app/component.py
+++ app/component.py
@@ -24,7 +24,7 @@ def page_style():
             /* Sidebar background with a dark overlay */
             [data-testid="stSidebar"] > div:first-child {{
                 background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
-                                  url("data:image/gif;base64,{sidebar_gif_base64}");
+                url("data:image/gif;base64,{sidebar_gif_base64}");
                 background-size: cover;
                 background-position: center;
                 background-repeat: no-repeat;
@@ -39,8 +39,8 @@ def page_style():
                 right: 2rem;
             }}
 
-            .stButton>button {{background-color: #4CAF50; color: white !important;}}
-            .stDownloadButton>button {{background-color: #4CAF50; color: white !important;}}
+            .stButton>button {{background-color: #003366; color: white !important;}}
+            .stDownloadButton>button {{background-color: #003366; color: white !important;}}
 
             /* Certification Card Styles */
             .cert-card {{
@@ -71,33 +71,52 @@ def page_style():
     # Sidebar content
     with st.sidebar:
         # Display the round profile picture at the top of the sidebar
-        st.image("photos/Background_Photo.png", width=150)
+        st.image("photos/Round_Profile_Photo.png", width=150)
 
         st.markdown("""
-            ## Fahmi Zainal
-            **Data Scientist | Analytics Engineer | Survey | Digital Marketing | Software Development | ML & AI | ETL | Databricks | API Deployment | Azure & AWS DevOps Practitioner | Army Reserved Officer 🎖**
+            ## Web Scraping Project
+            **This project focuses on scraping job listings from various online platforms, extracting details such as job title, company, location, and posting date. The aim is to analyze and visualize the data to gain insights into job market trends and opportunities.**
         """)
 
         st.markdown("""
-        ### Top Skills
-        - **Data Analytics, EDA and Modelling**
-        - **Power BI and Tableau Dashboarding**
+        ### Relevant Skills
+        - **Web Scraping Techniques (Beautiful Soup, Scrapy)**
+        - **Data Collection**
+        - **Data Profiling**
+        - **Data Analysis**
+        - **Data Visualization**
         - **Python (Programming Language)**
-        - **SQL Querying and Modelling**
-        - **Machine Learning (ML)**
-        - **RESTful APIs Designing**
-        - **Databricks**
-        - **AWS(S3, ECS, EC2)**
-        - **Google Cloud Platform (GCP) APIs**
-        - **TensorFlow and Pytorch Framework**
+        - **Streamlit,HTML,CSS**
         """)
 
         # HTML and JavaScript to open YouTube in a new tab
         new_tab_button = """
         <a href="https://www.youtube.com/watch?v=VeUiVCb7ZmQ?si=GzSBUP3zs1hEkigI" target="_blank">
-            <button style="background-color: #4CAF50; color: white; border: none; padding: 10px 20px; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
+            <button style="background-color: #003366; color: white; border: none; padding: 10px 20px; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
                 🎵 Play Music while Reading
             </button>
         </a>
         """
         st.markdown(new_tab_button, unsafe_allow_html=True)
+
+        st.markdown("""---""")
+
+        # LinkedIn button with logo
+        linkedin_url = "https://www.linkedin.com/in/fahmizainal17"
+        st.markdown(f"""
+            <a href="{linkedin_url}" target="_blank">
+                <button style="background-color: #0077B5; color: white; border: none; padding: 10px 20px; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1200px-LinkedIn_icon.svg.png" width="16" style="vertical-align: middle;"> Connect on LinkedIn
+                </button>
+            </a>
+        """, unsafe_allow_html=True)
+
+        # GitHub button with logo
+        github_url = "https://github.com/fahmizainal17"
+        st.markdown(f"""
+            <a href="{github_url}" target="_blank">
+                <button style="background-color: #333; color: white; border: none; padding: 10px 20px; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
+                    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="16" style="vertical-align: middle;"> Check out my GitHub
+                </button>
+            </a>
+        """, unsafe_allow_html=True)
